16187a02b800d55ec07205a560d4ab882b420d49
Colton Federighi <cmhfederighi@gmail.com>
2025-09-11 19:40:18 -0400

    Stylize, add functionality template
    Added filler buttons. Currently have nothing until I find a good cloud service provider.
    Changed table functionality.
    Added difference between local and server file actions.
    Made all buttons at the top an actual button instead of a P element.
    Made table more efficient.
    Other things I forgot.

--- static/js/fillTimes.js
+++ static/js/fillTimes.js
@@ -1,61 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
     const containerNames = ["mondayTimes", "tuesdayTimes", "wednesdayTimes", "thursdayTimes", "fridayTimes", "saturdayTimes"];
     for (let day = 0; day <= containerNames.length - 1; day++) {
+        // i'll try to clean up the reset of the program here by using functions: i have technology!
+        function makeP(text) {
+            const p = document.createElement("p")
+            p.textContent = text;
+            p.classList.add("timeItem");
+            return p;
+        }
+
         const container = document.getElementById(containerNames[day]);
         console.log("Filling times for " + containerNames[day] + "...");
         
         const labelDiv = document.createElement("div");
         labelDiv.style.backgroundColor = "hsl(0, 0%, 100%)";
         labelDiv.classList.add("horizontalGrid");
-        
-        const labelTimeP = document.createElement("p");
-        labelTimeP.textContent = "TIME";
-        labelTimeP.classList.add("timeItem");
-        labelDiv.append(labelTimeP);
+        labelDiv.style.gridAutoColumns = "200px 200px 1fr 200px";
+        labelDiv.style.paddingLeft = "5px";
+        labelDiv.style.paddingRight = "5px";
 
-        const labelApptBoolP = document.createElement("p");
-        labelApptBoolP.textContent = "APPT SET?";
-        labelApptBoolP.classList.add("timeItem");
-        labelDiv.append(labelApptBoolP);
-        
-        const labelApptNameP = document.createElement("p");
-        labelApptNameP.textContent = "APPT NAME";
-        labelApptNameP.classList.add("timeItem");
-        labelDiv.append(labelApptNameP);
+        labelDiv.append(makeP("TIME")); // generate the time labels
+        labelDiv.append(makeP("APPT SET?"));
+        labelDiv.append(makeP("APPT NAME"));
+        labelDiv.append(makeP("APPT TYPE"));
 
-        const labelApptTypeP = document.createElement("p");
-        labelApptTypeP.textContent = "APPT TYPE";
-        labelApptTypeP.classList.add("timeItem");
-        labelDiv.append(labelApptTypeP);
-
-        container.append(labelDiv);
+        container.append(labelDiv); // add the label to the top
         
         
-        for (let i = 0; i <= 35; i++) { // 35 is the amount of hours
+        for (let i = 0; i <= 35; i++) { // 35 is the amount of time periods we have. 35 * 6 = 210, so we have 210 total time periods across 6 days
 
             const timeDiv = document.createElement("div"); // this is our wrapper
-            timeDiv.style.backgroundColor = "hsl(" + ((20 * i) % 360) + ", 100%, 75%)"; // make the wrapper colored fancy
-            timeDiv.classList.add("horizontalGrid"); // make the wrapper arrange everything horizontally (i should really make this work with everything)
+            timeDiv.style.backgroundColor = "hsl(44, 22%, " + (i / 2 + 60 + (i % 2 == 0 ? 10 : 0)) + "%)"; // make the wrapper colored fancy
+            timeDiv.classList.add("horizontalGrid"); // make the wrapper arrange everything horizontally (i should really use this class with everything)
+            timeDiv.style.gridAutoColumns = "200px 200px 1fr 200px";
+            timeDiv.style.borderTop = "1px solid #000"
+            timeDiv.style.paddingLeft = "5px";
+            timeDiv.style.paddingRight = "5px";
 
-            const timeP = document.createElement("p"); // this holds the time for me. i'll make this the first column. god i hope i stick to this
-            timeP.textContent = ((Math.floor((i + 2) / 4 - 1) + 9) % 12 + 1) + ":" + (Math.floor((i + 2) % 4) * 15) + " to " + ((Math.floor((i + 3) / 4 - 1) + 9) % 12 + 1) + ":" + (Math.floor((i + 3) % 4) * 15);
-            timeP.classList.add("timeItem");
-            timeDiv.append(timeP);
+             // this holds the time for me. i'll make this the first column. god i hope i stick to this
+            const timeMinsNow = String((Math.floor((i + 2) % 4) * 15)).padStart(2, "0");
+            const timeHoursNow = String(((Math.floor((i + 2) / 4 - 1) + 9) % 12 + 1)).padStart(2, "0");
+            const timeMinsThen = String((Math.floor((i + 3) % 4) * 15)).padStart(2, "0");
+            const timeHoursThen = String(((Math.floor((i + 3) / 4 - 1) + 9) % 12 + 1)).padStart(2, "0");
+            timeDiv.append(makeP(timeHoursNow + ":" + timeMinsNow + " to " + timeHoursThen + ":" + timeMinsThen));
 
-            const apptBoolP = document.createElement("p"); // this shows the user whether an appointment is set up
-            apptBoolP.textContent = "No appointment set"; // dummy until i can find a cloud hosting service (supabase is a pain on linux)
-            apptBoolP.classList.add("timeItem");
-            timeDiv.append(apptBoolP); 
+            // this shows the user whether an appointment is set up
+            // dummy until i can find a cloud hosting service (supabase is a pain on linux)
+            timeDiv.append(makeP("No appointment set.")); 
 
-            const apptNameP = document.createElement("p"); // this shows the user who has the appointment set
-            apptNameP.textContent = "N/A" // dummy yet again
-            apptNameP.classList.add("timeItem");
-            timeDiv.append(apptNameP);
+            // this shows the user who has the appointment set
+            // dummy yet again
+            timeDiv.append(makeP("N/A"));
 
-            const apptTypeP = document.createElement("p"); // this shows the user what type of appointment is set (ex. nails, idk what else they have there)
-            apptTypeP.textContent = "N/A" // ditto
-            apptTypeP.classList.add("timeItem");
-            timeDiv.append(apptTypeP);
+            // this shows the user what type of appointment is set (ex. nails, idk what else they have there)
+            // ditto
+            timeDiv.append(makeP("N/A"));
 
             container.append(timeDiv); // write all changes
         }
